875315d18750a417202322ef391eac0bf8ee971e
vinod <vinodkumarbellamkonda@gmail.com>
2024-02-15 16:51:31 +0530

    Added codesmell test file

---
+++ Codesmaell.js
@@ -0,0 +1,32 @@
+function processUserData(user) {
+    // Validate user data
+    if (!user || !user.name || !user.email || !user.age) {
+      console.error("Invalid user data");
+      return;
+    }
+  
+    // Check user age
+    if (user.age < 18) {
+      console.log("User is underage");
+    } else {
+      // Process user data
+      console.log("Processing user data...");
+      // Do something with user data
+      // ...
+      // More code
+      // ...
+  
+      // Another block of code
+      // ...
+      // More code
+      // ...
+  
+      // Yet another block of code
+      // ...
+      // More code
+      // ...
+  
+      // And so on...
+    }
+  }
+  
